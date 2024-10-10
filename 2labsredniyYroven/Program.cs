f655eb8cfaf054ec08eb31e3fef37e8ebfd79018
Admin <Admin@DESKTOP-UGP0C2G>
2024-10-10 22:30:27 +0200

    Добавьте файлы проекта.

---
+++ 2labsredniyYroven/Program.cs
@@ -0,0 +1,40 @@
+﻿
+//
+//try
+//{
+//Console.WriteLine("Введите: a");
+//int a = int.Parse(Console.ReadLine());
+//Console.WriteLine("Введите: b");
+//int b = int.Parse(Console.ReadLine());
+//double y = Math.Pow(Math.Sin(a) + Math.Pow(Math.Tan(b),(3)), 2);
+//Console.WriteLine($"Ответ: = {y:F2}");
+
+//}
+//catch
+//{
+//}
+
+//
+//    try
+//{
+//    Console.WriteLine("Введите x: ");
+//    double x = double.Parse(Console.ReadLine()); 
+//    double a = Math.Sqrt(x) * Math.Abs(x);
+//    Console.WriteLine($"Ответ : {a:F2}");
+//}
+//catch
+//{
+//try
+//{
+//    Console.WriteLine("Введите x: ");
+//    double x = double.Parse(Console.ReadLine());
+//    Console.WriteLine("Введите m: ");
+//    double m = double.Parse(Console.ReadLine());
+//    double b = Math.Pow(x, 4) + Math.Pow(m, 2);
+//    Console.WriteLine($"Ответ : {b}");
+
+//}
+//catch
+//{
+
+//}
